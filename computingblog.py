1eb48600974c137240f39f9db60db8e1f695d5f5
Nishkrit Desai <nishkritdesai10@gmail.com>
2018-11-05 17:28:09 -0800

    Added routes to framework

--- computingblog.py
+++ computingblog.py
@@ -7,10 +7,40 @@ Description:
 Author: ndesai (Nishkrit)
 """
 
-from flask import Flask
+import sys
 
-def main():
-    return 0
+from flask import Flask, render_template
+from flask_flatpages import FlatPages, pygments_style_defs
+from flask_frozen import Freezer
 
-if __name__=='__main__':
-    main()
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+FLATPAGES_ROOT = 'content'
+POST_DIR = 'posts'
+
+app = Flask(__name__)
+flatpages = FlatPages(app)
+freezer = Freezer(app)
+app.config.from_object(__name__)
+
+
+@app.route("/")
+def index():
+    posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
+    posts.sort(key=lambda item: item['date'], reverse=False)
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/posts/<name>/')
+def post(name):
+    path = '{}/{}'.format(POST_DIR, name)
+    post = flatpages.get_or_404(path)
+    return render_template('post.html', post=post)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(host='0.0.0.0', debug=True)
