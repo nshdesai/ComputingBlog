d0250584293b45e1e3466334970570fe15e80056
Nishkrit Desai <nishkritdesai10@gmail.com>
2018-11-11 19:13:17 -0800

    Updated ssl context

--- computingblog.py
+++ computingblog.py
@@ -61,4 +61,4 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
         freezer.freeze()
     else:
-        app.run(host='0.0.0.0', debug=True)
+        app.run(ssl_context='adhoc')
